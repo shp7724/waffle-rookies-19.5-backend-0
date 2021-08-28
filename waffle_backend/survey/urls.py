738edccc2ff6ab8746fb21aad6691ce24710e682
shp7724 <shp7724@naver.com>
2021-08-28 18:03:12 +0900

    과제 완료

--- waffle_backend/survey/urls.py
+++ waffle_backend/survey/urls.py
@@ -5,4 +5,6 @@ from survey import views
 urlpatterns = [
     path('results/', views.get_survey_results, name="get_surveys"),
     path('results/<survey_id>/', views.get_survey, name="get_survey"),
+    path('os/', views.list_all_os, name="list_all_os"),
+    path('os/<os_id>', views.get_os, name="get_os"),
 ]
--- waffle_backend/survey/views.py
+++ waffle_backend/survey/views.py
@@ -1,15 +1,19 @@
-import json
 from django.http import JsonResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 
-from survey.models import SurveyResult
-from survey.serializers import serialize_survey_result
+from survey.serializers import *
 
 
 def get_survey_results(request):
     if request.method == 'GET':
         params = request.GET.get('os')
-        survey_results = list(map(lambda result: serialize_survey_result(result), SurveyResult.objects.all()))
+        if params not in ['Windows', 'MacOS', 'Linux']:
+            return JsonResponse(None, status=400, safe=False)
+        if params is None:
+            data = SurveyResult.objects.all()
+        else:
+            data = SurveyResult.objects.filter(os__name=params)
+        survey_results = list(map(lambda result: serialize_survey_result(result), data))
         return JsonResponse({"surveys": survey_results}, status=200)
     else:
         return HttpResponseNotAllowed(['GET', ])
@@ -19,5 +23,24 @@ def get_survey(request, survey_id):
     if request.method == 'GET':
         survey = get_object_or_404(SurveyResult, id=survey_id)
         return JsonResponse(serialize_survey_result(survey))
+    else:
+        return HttpResponseNotAllowed(['GET', ])
+
+def list_all_os(request):
+    if request.method == 'GET':
+        data = [serialize_os(os) for os in OperatingSystem.objects.all()]
+        return JsonResponse(data, status=200, safe=False)
+    else:
+        return HttpResponseNotAllowed(['GET', ])
+
+def get_os(request, os_id):
+    if request.method == 'GET':
+        try:
+            data = serialize_os(OperatingSystem.objects.get(pk=os_id))
+        except OperatingSystem.DoesNotExist:
+            data = None
+            return JsonResponse(data, status=404, safe=False)
+        else:
+            return JsonResponse(data, status=200)
     else:
         return HttpResponseNotAllowed(['GET', ])
